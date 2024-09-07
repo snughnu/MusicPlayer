f75eeb61369c725c70220c934525ad7063c79d2e
snughnu <ksnughnu@gmail.com>
2024-09-07 20:34:09 +0900

    viewdidload에 코드 한 줄 추가

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -165,6 +165,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.addViewsWithCode()
         self.initializePlayer()
     }
     
